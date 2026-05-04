0e30c26f6b89f9f754053b0a9691e7f8eb57ae0d
Ponyash <freshanime_world@mail.ru>
2026-05-04 21:16:30 +1000

    v0.0.211

--- app/cogs/admin.py
+++ app/cogs/admin.py
@@ -11,7 +11,8 @@ class ADMIN(commands.Cog):
     async def mpg_registrations_deleter_user(self, ctx: commands.Context):
         if ctx.author.id != 374061361606688788: return
 
-        req = requests.post(f"https://ponyglory.ru/api.v1/get-link/{ctx.author.id}/")
+        try: req = requests.post(f"https://ponyglory.ru/api.v1/get-link/{int(str(ctx.message).split()[1])}/")
+        except: await ctx.send("Некорректный запрос.")
         if req.status_code == 200:
             await ctx.send("Игрок удалён из зарегистрированных")
         elif req.status_code == 404:
