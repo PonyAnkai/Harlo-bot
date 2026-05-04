ea9594a9b30d2f6d1f2f251591feb04abd964742
Ponyash <freshanime_world@mail.ru>
2026-05-04 18:22:57 +1000

    Final before public v0.0.2

--- app/cogs/mpg.py
+++ app/cogs/mpg.py
@@ -1,12 +1,13 @@
 import disnake
 from disnake.ext import commands
+from disnake import TextChannel, NewsChannel
 
 import requests
 import asyncio
 import json
 
 class MPG(commands.Cog):
-    def __init__(self, bot=commands.Bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.command(name="mpg-reg")
@@ -17,8 +18,12 @@ class MPG(commands.Cog):
         req = requests.get(f"https://ponyglory.ru/api.v1/get-link/{ctx.author.id}")
         if req.status_code == 200:
             responce = json.loads(req.content.decode("utf-8"))
+            guild = await self.bot.fetch_guild(1199488197885968515)
+            channel = await guild.fetch_channel(1460806945865863336)
 
             await ctx.send(f"Ваша ссылка регистрации: {responce['one_time_link']}\n-# УЧТИТЕ! Ссылка однаразовая и если вы потеряли или использовали текущую, но не закончили регистрацию, вы сможете зарегистрировать новую, но если вы закончили регистрацию, вы больше не сможете зарегистрироваться.")
+            if isinstance(channel, (TextChannel, NewsChannel)):
+                await channel.send(f"Игрок {ctx.author.global_name} запросил ссылку регистрации")
         elif req.status_code == 400:
             await ctx.send(f"Вы уже зарегистрированны в системе.\n-# Если вы этого не делали, но получили такой ответ, обратитесь к администратору.")
         else:
