981165a1b9a1372beac01ccc7882872afe8c76cf
Ponyash <freshanime_world@mail.ru>
2026-06-18 18:49:07 +1000

    v0.0.33

--- app/cogs/mpg.py
+++ app/cogs/mpg.py
@@ -34,7 +34,7 @@ class MPG(commands.Cog):
 
         req = requests.get(f"http://127.0.0.1:8090/api/v1/private/base-ankets/{user_uid}")
         if req.status_code == 200:
-            file_stream = io.BytesIO(req.content)
+            file_stream = io.BytesIO(req.content.decode("utf-8").encode("utf-8"))
             discord_file = disnake.File(fp=file_stream, filename="UserData.json")
             await ctx.send(file=discord_file)
         else:
